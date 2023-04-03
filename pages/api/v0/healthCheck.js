7c3a881663cb9d26d3cff262f76e656a835d78c6
Ajay Sangamithran <psajay1994@gmail.com>
2023-04-04 03:22:01 +0530

    code cleanup

--- pages/api/v0/healthCheck.js
+++ pages/api/v0/healthCheck.js
@@ -1,9 +1,11 @@
 // Health Check for Monitoring systems
 
-export default async (req, res) => {
+const check = async (req, res) => {
   if (req.method == "POST") {
     res.status(200).json({ message: "I'm up and running" });
   } else if (req.method == "GET") {
     res.status(200).json({ message: "I'm up and running" });
   }
 };
+
+export default check;
